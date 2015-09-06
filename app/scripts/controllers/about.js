77a1dc196647e4d52bc306aaab7981c52e49bfc0
unknown <Ajit_Menon@TVMATP270792D.ad.infosys.com>
2015-09-06 16:47:34 +0530

    test

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name smartvillageApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the smartvillageApp
+ */
+angular.module('smartvillageApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name smartvillageApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the smartvillageApp
+ */
+angular.module('smartvillageApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
